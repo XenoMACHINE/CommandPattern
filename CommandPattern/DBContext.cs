8b63327e3cb09b773fcf628d0d948285b52d7c47
Alexandre Ménielle <alexandre.menielle@gmail.com>
2018-05-01 14:09:52 +0200

    implement command pattern test

---
+++ CommandPattern/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CommandPattern
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source =database.db");
+        }
+    }
+}
